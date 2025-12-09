7a2b8a3d8a5d6f582507641d028a47144de1f844
Frank Fasbender <ffasbend@gmail.com>
2025-12-09 22:36:11 +0100

    Deploy on gh-pages updated

--- assets/javascript-Bq9bz6CO.js
+++ assets/javascript-Bq9bz6CO.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-mAzTUzgi.js";import"./index-DiifP9eY.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-BOLNAXz7.js";import"./index-Br-3C8B1.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.47.0(69991d66135e4a1fc1cf0b1ac4ad25d429866a0d)
  * Released under the MIT license
